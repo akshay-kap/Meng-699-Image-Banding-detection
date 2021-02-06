505e87458235a84217bbb79674e5415f88014a3f
AKSHAY KAPOOR <a43kapoo@uwaterloo.ca>
2021-02-06 17:23:22 -0500

    Updated L6 with single step

--- Dataset-Generation/Generating-Quantized-frames-from-videos/L6_Single_Step.py
+++ Dataset-Generation/Generating-Quantized-frames-from-videos/L6_Single_Step.py
@@ -8,7 +8,6 @@ class VideoCaptureYUV:
 	def __init__(self, filename, size):
 		self.height, self.width = size
 		self.frame_len = int(self.width * self.height * 3 / 2)
-		#print(self.frame_len)
 		self.f = open(filename, 'rb')
 		#self.f_new=open("test111.yuv", 'wb+')
 		self.shape = (int(self.height*1.5), self.width)
@@ -23,10 +22,6 @@ class VideoCaptureYUV:
 		y_comp=np.clip(y_comp+step,a_min = 2, a_max = 255)
 		return y_comp        
 	def read_raw(self):
-#         raw = self.f.read(2*self.frame_len)
-#         yuv = np.frombuffer(raw, dtype=np.uint16)
-#         print(np.shape(yuv))
-#         yuv = yuv.reshape(self.shape)
 		try:
 			raw = self.f.read(self.frame_len)
 			yuv = np.frombuffer(raw, dtype=np.uint8)
@@ -42,20 +37,13 @@ class VideoCaptureYUV:
 			return ret, yuv
 		y=yuv[:1080,:]
 		uv=yuv[1080:,:]
-		# y=self.adjust_luminance(y,step=lum_step)
 		y=self.quantize(y,6,8)
-		#uv=self.quantize(uv,5,8)
 		y=self.quantize_inverse(y,6,8)
-		#uv=self.quantize_inverse(uv,5,8)
-		
 		yuv=np.concatenate((y,uv),axis=0)
 
 		yuv_mod = yuv.reshape(self.frame_len,)
-		#print(np.shape(yuv_mod))
-		#raw_quant = self.f_new.write(bytearray(yuv_mod))
 		bgr = cv2.cvtColor(yuv, cv2.COLOR_YUV2RGB_I420)
 		rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
-		# gray=gray = cv2.cvtColor(yuv, cv2.COLOR_BGR2GRAY)
 		return ret, rgb
 
 def step1():
@@ -66,9 +54,6 @@ def step1():
 	for name in files:
 		cmd="ffmpeg -i {0} -c:v rawvideo -pix_fmt yuv420p {1}".format(inputfilepath+"/"+name,outputfilepath+"/"+name[:-3]+"yuv") 
 		check_output(cmd, shell=True).decode()
-		print(cmd)
-		#os.remove(inputfilepath+"/"+name)
-	print("shukar hai!")
 	print("Step 1 completed")
 	for name in files:
 		os.remove(inputfilepath+"/"+name)
@@ -79,24 +64,13 @@ def step2():
 	files=os.listdir(path)
 	for name in files:
 		filename=path+'\\'+name
-		# filename="0165_fps30.yuv"
-		print(filename)
-		# size = (1080, 1920)
-		# cap = VideoCaptureYUV(filename, size)
-		# fourcc=cv2.VideoWriter_fourcc(*'MP4V')
-		# fourcc=0x7634706d
-		# fps=int(name[-6:-4])
-		# print("hey...",fps)
-		
 		for lum_step in range(0,1):
 			size = (1080, 1920)
 			cap = VideoCaptureYUV(filename, size)
 			fourcc=cv2.VideoWriter_fourcc(*'MP4V')
 			fourcc=0x7634706d
 			fps=int(name[-6:-4])
-			# print("fps...",fps)
 			out=cv2.VideoWriter('./mp4/{0}_6b.mp4'.format(name[:-4]),fourcc,fps,(1920,1080))
-			#print(lum_step)
 			while 1:
 				ret, frame = cap.read(lum_step)
 				out.write(frame)
@@ -105,12 +79,9 @@ def step2():
 					pass
 				else:
 					break
-		print("a file done")     
+   
 	print("step 2 completed")
-
-	#cv2.destroyAllWindows()
-
-
+	
 def step3():
 	#step3
 	path = "./yuv"
@@ -132,17 +103,15 @@ def step4():
 			print(cmd)
 		except:
 			pass
-	print("shukar hai!")
-	print("step 3 completed")
+	print("step 4 completed")
 
 
 if __name__ == "__main__":
-	print("oh yes we started")
 	step1()
 	step2()
 	step3()
 	step4()
-	print("all done")
+
 
 
 
